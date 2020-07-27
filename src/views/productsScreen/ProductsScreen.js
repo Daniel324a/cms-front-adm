2943f5b539501df236aa86d2c356e11ab57891b9
dylanbatar <dylanbatar2@gmail.com>
2020-07-27 12:08:27 -0500

    Cambio en los botones de accion

--- src/views/productsScreen/ProductsScreen.js
+++ src/views/productsScreen/ProductsScreen.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { ProductCard } from "../../components/cards/ProductCard";
+import React from 'react';
+import { ProductCard } from '../../components/cards/ProductCard';
 
-import "./productsScreen.css";
+import './productsScreen.css';
 
 export const ProductsScreen = () => {
   const cards = [];
@@ -11,38 +11,58 @@ export const ProductsScreen = () => {
 
   return (
     <div>
-      <h1 className="h3 mb-0 text-gray-800">Administrador de Productos</h1>
-      <div className="input-group mb-2 mt-3">
+      <h1 className='h3 mb-0 text-gray-800'>Administrador de Productos</h1>
+      <div className='input-group mb-2 mt-3'>
         <input
-          type="text"
-          className="form-control"
-          placeholder="Nombre de producto..."
-          aria-label="Nombre de producto..."
-          aria-describedby="button-addon2"
+          type='text'
+          className='form-control'
+          placeholder='Nombre de producto...'
+          aria-label='Nombre de producto...'
+          aria-describedby='button-addon2'
         />
-        <div className="input-group-append">
+        <div className='input-group-append'>
           <button
-            className="btn btn-outline-primary"
-            type="button"
-            id="button-addon2"
+            className='btn btn-outline-primary'
+            type='button'
+            id='button-addon2'
           >
             Search
-            <i className="fas fa-fw fa-search ml-1"></i>
+            <i className='fas fa-fw fa-search ml-1'></i>
           </button>
         </div>
       </div>
-      <div className="row ml-1">
-        <button type="button" className="btn btn-danger btn-sm mr-2 mb-2">
-          Añadir Producto
+      <div className='row ml-1'>
+        <button
+          type='button'
+          class='btn btn-primary btn-sm mr-2 mb-2 btn-icon-split'
+        >
+          <span class='icon text-white-50'>
+          <i class="fas fa-plus-square"></i>
+          </span>
+          <span class='text'>Añadir Producto</span>
         </button>
-        <button type="button" className="btn btn-warning btn-sm mr-2 mb-2">
-          Editar Producto
+
+        <button
+          type='button'
+          class='btn btn-warning btn-sm mr-2 mb-2 btn-icon-split'
+        >
+          <span class='icon text-white-50'>
+            <i class='fas fa-edit'></i>
+          </span>
+          <span class='text'> Editar Producto</span>
         </button>
-        <button type="button" className="btn btn-secondary btn-sm mr-2 mb-2">
-          Eliminar Producto
+
+        <button
+          type='button'
+          class='btn btn-danger btn-sm mr-2 mb-2 btn-icon-split'
+        >
+          <span class='icon text-white-50'>
+            <i class='fas fa-trash'></i>
+          </span>
+          <span class='text'> Eliminar Producto</span>
         </button>
       </div>
-      <div className="cards-grid row">{cards}</div>
+      <div className='cards-grid row'>{cards}</div>
     </div>
   );
 };
